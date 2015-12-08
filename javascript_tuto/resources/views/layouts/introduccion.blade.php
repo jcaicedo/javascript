acae5c61bc0d075d8a634a77e9b9b6b8bcd97b32
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2015-12-08 18:26:11 -0430

    capitulo 1

---
+++ javascript_tuto/resources/views/layouts/introduccion.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Introduccion JavaScript</title>
+
+</head>
+<body>
+
+
+
+<h1>Mi sitio Web</h1>
+
+
+<script src="js/intro.js"></script>
+</body>
+</html>
