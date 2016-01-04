a48358ac59c4a614aa3fe6f3e8d18327100a0b31
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2016-01-04 17:37:31 -0430

    capitulo 18

--- javascript_tuto/resources/views/cap18.blade.php
+++ javascript_tuto/resources/views/cap18.blade.php
@@ -2,47 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cap17 DOM Document Object Model</title>
+    <title>Cap18 DOM Document Object Model Modificando Estilos de los elementos</title>
+
+    {!! Html::style('css/cap18.css') !!}
 </head>
 <body>
-
-<!--<ol id="lista">
-    <li id="primero"> Lorem ipsum <b> dolor </b> sit amet.</li>
-    <li>Lorem ipsum dolor sit amet.</li>
-</ol>-->
+    <h1 id="encabezado" class="titulo tituloGrande">Soy un encabezado</h1>
 
 
-<ol id="lista2">
-    <li id="segundo"> Lorem ipsum <b> dolor </b> sit amet.</li>
-    <li>Lorem ipsum dolor sit amet.</li>
-</ol>
-
-<script>
- /*   var elemento = document.createElement("li"),
-            contenido = document.createTextNode("Nuevo Texto");
-
-    elemento.appendChild(contenido);*/
-
-/*
-    var primerElemento = document.getElementById("primero");
-   //primerElemento.innerHTML="NUevo Textp <i>Julio Caicedo<i>";// reemplaza el elemento de un elemento html y ejecuta codigo html
-    //primerElemento.textContent = "NUevo Textp <i class='foo'>Julio Caicedo<i>"; // reemplaza pero no ejecuta codigo html
-    primerElemento.innerHTML = "NUevo Textp <i class='foo'>Julio Caicedo<i>";
-*/
 
 
- var elemento = document.createElement("li"),
-         contenido = document.createTextNode("Nuevo Texto");
+    <script>
 
- elemento.appendChild(contenido);
-    var padre = document.getElementsByTagName("li")[0].parentNode;
-    var referencia =document.getElementsByTagName("li")[0];
-  //  padre.replaceChild(elemento,referencia);//reemplaza elementos
+        var encabezado = document.getElementById("encabezado");
+      //  encabezado.style.color="#fff";
+      //  encabezado.style.background="navy";
+      //  encabezado.style.paddingTop = "20px";
 
-    padre.removeChild(referencia); //eliminar elementos
+       encabezado.className = encabezado.className.replace("tituloGrande","");
 
-</script>
-
+    </script>
 
 </body>
 </html>
