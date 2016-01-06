9155562e37c9a8a2b747b95b517232c94e1322c3
julio <caicedo.julio@gmail.com>
2016-01-06 00:06:07 -0430

    cap25

--- javascript_tuto/resources/views/cap25.blade.php
+++ javascript_tuto/resources/views/cap25.blade.php
@@ -2,16 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cap25 Reloj Digital</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Cap25 Reloj Digital de 12 horas - JS</title>
+    {!! Html::style('css/cap25.css')!!}
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+
 
 </head>
 <body>
 
+<div class="wrap">
+    <div class="widget">
+        <div class="fecha">
+            <p id="diaSemana" class="diaSemana"></p>
+            <p id="dia" class="dia"></p>
+            <p>de</p>
+            <p id="mes" class="mes"></p>
+            <p>del</p>
+            <p id="year" class="year"></p>
+        </div>
+        <div class="reloj">
+            <p id="horas" class="horas"></p>
+            <p>:</p>
+            <p id="minutos" class="minutos"></p>
+            <p>:</p>
+            <div class="caja-segundos">
+                <p id="ampm" class="ampm"></p>
+                <p id="segundos" class="segundos"></p>
 
-<script>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-</script>
-
+<script src="js/cap25.js"></script>
 </body>
 </html>
