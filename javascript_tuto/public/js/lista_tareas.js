0cc2c1f3d8437511d6dbaf4c73fbb726d3a0b14e
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2016-01-07 10:52:56 -0430

    1/2 code javascript Listar Tareas

--- javascript_tuto/public/js/lista_tareas.js
+++ javascript_tuto/public/js/lista_tareas.js
@@ -1,5 +1,42 @@
 (function(){
 
+    //Variables
 
+    var lista = document.getElementById("lista"),
+        tareaInput = document.getElementById("tareaInput"),
+        btnNuevaTarea = document.getElementById("btn-agregar");
+
+    //Funciones
+
+    var agregarTarea = function () {
+        alert("Agregar Tarea");
+
+    };
+    var comprobarInput = function(){
+        alert("Comprobar Input");
+
+    };
+    var eliminarTarea = function(){
+        alert("Eliminar Tarea");
+
+    };
+
+    //Eventos
+
+        //AgregarTarea
+
+    btnNuevaTarea.addEventListener("click", agregarTarea);
+
+        //Comprobar Input
+
+    tareaInput.addEventListener("click", comprobarInput);
+
+    //Borrando Elementos de la lista
+    for (var i=0; i<= lista.children.length -1;i++){
+
+        lista.children[i].addEventListener("click", eliminarTarea);
+
+
+    }
 
 }())
