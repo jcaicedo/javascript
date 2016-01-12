c1ffb8e96c772628047584d38a9e6f38393ff010
Julio Caicedo <juliocaicedo@Bees-MacBook-Pro.local>
2016-01-12 16:15:02 -0430

    no message

--- javascript_tuto/public/js/calculadora.js
+++ javascript_tuto/public/js/calculadora.js
@@ -1,6 +1,7 @@
 var cifra="";
 var acumulado =0;
 var sumar = false;
+var restar = false;
 
 function display_number(numero){
 
@@ -20,9 +21,33 @@ function suma(){
     sumar= true;
 }
 
+
+function resta(){
+
+    if (sumar){
+        acumulado = acumulado+parseInt(cifra);
+        document.getElementById("display").value=acumulado;
+    }else{
+
+    acumulado = acumulado-parseInt(cifra);
+    document.getElementById("display").value=acumulado;
+    console.log(acumulado);
+    }
+
+    cifra = "";
+    sumar=false;
+    restar= true;
+
+
+}
+
+
 function resultado(){
     if(sumar){
         document.getElementById("display").value=acumulado+parseInt(cifra);
+    }else if(restar){
+
+        document.getElementById("display").value=acumulado-parseInt(cifra);
     }
 
 }
